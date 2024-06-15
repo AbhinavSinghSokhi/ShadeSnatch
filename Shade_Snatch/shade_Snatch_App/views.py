8f69e51d2273fdcd6de16815da248a42dae152df
Abhinav Singh <sokhiabhinavsingh@gmail.com>
2024-06-15 21:30:01 +0530

    banner ui done

--- Shade_Snatch/shade_Snatch_App/views.py
+++ Shade_Snatch/shade_Snatch_App/views.py
@@ -8,9 +8,6 @@ from collections import Counter
 from skimage.color import rgb2lab, deltaE_cie76
 import os
 # Create your views here.
-def index(request):
-    return render(request,"index.html")
-
 def RGB2HEX(color):
     return "#{:02x}{:02x}{:02x}".format(int(color[0]), int(color[1]), int(color[2]))
 
@@ -35,6 +32,47 @@ def get_colors( image, number_of_colors, show_chart):
     # rgb_colors = [ordered_colors[i] for i in counts.keys()]
 
     return hex_colors
+
+
+def index(request):
+    if request.method == 'POST':
+        uploaded_image = request.FILES.get('image')
+        if uploaded_image:
+            image_upload = Image.objects.create(image=uploaded_image)
+            number = image_upload.number  # Retrieve the auto-incremented number
+            # print(number)
+            image_path= image_upload.image.url
+            image_path_for_html= image_path[1:]
+            print("Media path :",image_path_for_html)
+            path="/_Coding/openCV/Projects/real_world_projects/ShadeSnatch/Shade_Snatch/shade_Snatch_App/static"
+            # image_absolute_path = os.path.join(settings.MEDIA_ROOT,image_path)
+            image_absolute_path= str("E:"+path+image_path)
+            # print(image_absolute_path)
+            # img= cv.imread(f"{image_absolute_path}")
+
+            dominent_colours= get_colors(get_image(f"{image_absolute_path}"), 20, True)
+            # print(dominent_colours)
+            output={
+                'hexcodes': dominent_colours,
+                'image_path': image_path_for_html
+            }
+            return render(request,'index.html', {'output': output})
+    else:
+        path= "/_Coding/openCV/Projects/real_world_projects/ShadeSnatch/Shade_Snatch/shade_Snatch_App/static/images/banner-bg.jpg"
+        image_absolute_path= str("E:"+path)
+
+        # image_path_for_html= image_path[1:]
+        image_path_for_html= "./../static/images/banner-bg.jpg"
+        # print(image_absolute_path)
+        # img= cv.imread(f"{image_absolute_path}")
+
+        dominent_colours= get_colors(get_image(f"{image_absolute_path}"), 20, True)
+            # print(dominent_colours)
+        output={
+            'hexcodes': dominent_colours,
+            'image_path': image_path_for_html
+        }
+        return render(request,'index.html', {'output': output})
 
 def image_processing(request):
     if request.method == 'POST':
